946a5ee5ca6049eeed028a4fab7328877858388e
Patrick Gleeson <patrick.c.gleeson@googlemail.com>
2016-09-16 11:55:50 +0100

    Working test of throwaway class

---
+++ ThinginessSwift/Classes/Throwaway.swift
@@ -0,0 +1,14 @@
+//
+//  Throwaway.swift
+//  Pods
+//
+//  Created by Patrick Gleeson on 16/09/2016.
+//
+//
+
+public class Throwaway {
+    public var setToFoo: String
+    public init() {
+        setToFoo = "foo"
+    }
+}
